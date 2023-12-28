34d73e8640c8183a6f6a98de873498340131c067
Ananya-hyniva <147624126+hcanany@users.noreply.github.com>
2023-12-28 11:17:30 +0530

    testing

---
+++ src/Components/HeroSection/AboutSection/AboutComponent.js
@@ -0,0 +1,19 @@
+import { styled } from "@mui/system";
+
+const Bgwrapper =  styled('div')({
+  color: 'yellow',
+  height:'100vh',
+  
+  backgroundColor: 'black'
+
+
+})
+const  AboutComponent = () => {
+  return (
+    <Bgwrapper>
+        <h1>About Me</h1>
+    </Bgwrapper>
+  
+  )
+}
+export default AboutComponent;
